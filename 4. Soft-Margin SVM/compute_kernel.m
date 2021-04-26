a03ccf6797f72f049e9f1e6c1d338488fa016c43
Ivan Isakov <isakovi@bu.edu>
2021-04-26 17:32:07 -0400

    Update

--- 4. Soft-Margin SVM/compute_kernel.m
+++
@@ -1,6 +0,0 @@
-function [K] = compute_kernel(X,gamma)
-normX = full(sum(X.^2,2));
-K = repmat(normX,1,size(X,1)) + repmat(normX',size(X,1),1) - 2*full(X*X');
-K=exp(-K*gamma);
-end
-
--- 8. DNN/README.md
+++
@@ -1,1 +0,0 @@
-# <ins>Problem: DNN</ins>
